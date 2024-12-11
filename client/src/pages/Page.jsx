5c8d9df63a87a08b73c42c7047ddcc9bd2c62b1c
reagan13 <reaganmabawad@gmail.com>
2024-12-12 07:45:44 +0800

    update

--- client/src/pages/Page.jsx
+++ client/src/pages/Page.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import CustomTable from "../components/CustomTable";
-const Page = ({ name, response, categorization, analysis }) => {
+const Page = ({ name, categoryResponse, text, intentResponse }) => {
+	console.log("Page", categoryResponse, intentResponse);
 	return (
 		<div className="p-6 bg-white rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl border border-gray-200">
 			<div className="text-2xl font-bold mb-2 text-blue-600 text-center">
@@ -13,15 +14,15 @@ const Page = ({ name, response, categorization, analysis }) => {
 				</h2>
 				<div className="flex items-center justify-between mt-2">
 					<span className="text-gray-700">NER</span>
-					<span className="text-gray-700">70%</span>
+					<span className="text-gray-700">Unknown</span>
 				</div>
 				<div className="flex items-center justify-between mt-1">
 					<span className="text-gray-700">Category</span>
-					<span className="text-gray-700">20%</span>
+					<span className="text-gray-700">{categoryResponse.data.class}</span>
 				</div>
 				<div className="flex items-center justify-between mt-1">
 					<span className="text-gray-700">Intent</span>
-					<span className="text-gray-700">10%</span>
+					<span className="text-gray-700">{intentResponse.data.class}</span>
 				</div>
 			</div>
 			<div className="mb-4">
@@ -34,17 +35,10 @@ const Page = ({ name, response, categorization, analysis }) => {
 				<h2 className="text-lg font-semibold text-gray-800 border-b-2 border-blue-500 pb-1">
 					Response
 				</h2>
-				<p className="text-gray-700 mt-2">{response}</p>
+				<p className="text-gray-700 mt-2">{text}</p>
 			</div>
 		</div>
 	);
 };
 
 export default Page;
-
-Page.propTypes = {
-	name: PropTypes.string,
-	response: PropTypes.string,
-	categorization: PropTypes.string,
-	analysis: PropTypes.string,
-};
