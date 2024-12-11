8e86d451b41f9551fd8a4920930553b6eb94ff93
reagan13 <reaganmabawad@gmail.com>
2024-12-12 07:44:55 +0800

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import { DataProvider } from "./context/DataContext";
 
 const App = () => {
 	return (
-		<HeaderProvider>
-			<div className="flex flex-col h-screen bg-white">
-				<Header /> {/* Header now uses context */}
-				<div className="h-full w-full">
-					<Outlet />
+		<DataProvider>
+			<HeaderProvider>
+				<div className="flex flex-col h-screen bg-white">
+					<Header /> {/* Header now uses context */}
+					<div className="h-full w-full">
+						<Outlet />
+					</div>
 				</div>
-			</div>
-		</HeaderProvider>
+			</HeaderProvider>
+		</DataProvider>
 	);
 };
 
