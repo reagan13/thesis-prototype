e0d2e0411ab968f0cc9ba122be5374adf63fcb30
reagan13 <reaganmabawad@gmail.com>
2024-12-12 07:45:17 +0800

    updatev

---
+++ client/src/components/InputSection.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import { Send } from "lucide-react";
+const InputSection = ({ input, setInput, handleSend }) => (
+	<div className="flex justify-between items-center gap-4">
+		<input
+			type="text"
+			value={input}
+			onChange={(e) => setInput(e.target.value)}
+			placeholder="Enter your message..."
+			className="rounded-full w-full border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 py-4 px-5"
+		/>
+		<button
+			onClick={handleSend}
+			className="bg-blue-500 text-darkNavy rounded-full p-2 hover:bg-blue-600 transition duration-200"
+		>
+			<Send />
+		</button>
+	</div>
+);
+InputSection.propTypes = {
+	input: PropTypes.string,
+	setInput: PropTypes.func,
+	handleSend: PropTypes.func,
+};
+
+export default InputSection;
