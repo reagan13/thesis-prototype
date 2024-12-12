adff7899a3c6fe94723aa307c1dafa411f9a4c91
kim leones <k.leones.529601@umindanao.edu.ph>
2024-12-12 10:13:13 +0800

    Updated chatbot Interface

--- client/src/components/BotMessage.jsx
+++ client/src/components/BotMessage.jsx
@@ -1,33 +1,35 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 const BotMessage = ({ text, category, intent, ner, probabilities, id }) => {
-	return (
-		<div className="text-left">
-			<div className="bg-gray-300 p-2 rounded-lg inline-block space-y-8">
-				<div>
-					<p>Category: {category}</p>
-					<p>Intent: {intent}</p>
-					<p>Named Entity Recognition: {ner}</p>
-					<p>{text}</p>
-				</div>
-				<Link
-					to={{
-						pathname: `/distilbert/${id}`,
-					}}
-					className="bg-blue text-white p-2 rounded-lg text-center" // Add text-center for better alignment
-				>
-					View More
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className="text-left">
+      <div className="bg-gray-300 p-4 rounded-lg inline-block space-y-4">
+        <div>
+          <p>Category: {category}</p>
+          <p>Intent: {intent}</p>
+          <p>Named Entity Recognition: {ner}</p>
+          <p>{text}</p>
+        </div>
+        <div className="pb-2">
+          <Link
+            to={{
+              pathname: `/distilbert/${id}`,
+            }}
+            className=" text-gray-900 rounded-lg text-center hover:text-gray-600 transition duration-150 font-bold " // Add text-center for better alignment
+          >
+            View More &gt;
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 BotMessage.propTypes = {
-	text: PropTypes.string,
-	category: PropTypes.string,
-	intent: PropTypes.string,
-	ner: PropTypes.string,
-	id: PropTypes.number,
+  text: PropTypes.string,
+  category: PropTypes.string,
+  intent: PropTypes.string,
+  ner: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export default BotMessage;
--- client/src/components/UserMessage.jsx
+++ client/src/components/UserMessage.jsx
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
 const UserMessage = ({ text }) => (
-	<div className="text-right">
-		<div className="bg-blue text-white p-2 rounded-lg inline-block">{text}</div>
-	</div>
+  <div className="text-right mt-2">
+    <div className="bg-blue max-w-[400px] text-left text-white p-3 rounded-lg inline-block">
+      {text}
+    </div>
+  </div>
 );
 UserMessage.propTypes = {
-	text: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default UserMessage;
